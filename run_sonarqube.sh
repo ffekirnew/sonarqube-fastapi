ab5cdf994fa6f53d8b0ac854f688bd3852c08cd4
ffekirnew <ffekirnew0808@gmail.com>
2025-05-24 11:48:56 +0300

    Change sonarqube config

--- run_sonarqube.sh
+++ run_sonarqube.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-PROJECT_BASE_DIR=task_manager
 SONARQUBE_URL=http://localhost:9000
+PROJECT_BASE_DIR=sonarqube-fastapi
 SONARQUBE_PROJECT_KEY=fastapi
 SONARQUBE_TOKEN=sqp_f03c2bf4e9bd3319f6efb5139518338a3b318811
 
+pwd
+ls .
+mkdir -p /opt/sonar-scanner/conf
+ls /opt/sonar-scanner/conf
+cp sonar-project.properties /opt/sonar-scanner/conf/sonar-scanner.properties
+cat /opt/sonar-scanner/conf/sonar-scanner.properties
 
 docker run \
 		--network=host \
+    --platform linux/amd64 \
 		-v $PWD:/$PROJECT_BASE_DIR \
 		-e SONAR_HOST_URL="$SONARQUBE_URL" \
 		-e SONAR_SCANNER_OPTS="-Dsonar.projectKey=$SONARQUBE_PROJECT_KEY -Dsonar.login=$SONARQUBE_TOKEN -Dsonar.projectBaseDir=/$PROJECT_BASE_DIR" \
