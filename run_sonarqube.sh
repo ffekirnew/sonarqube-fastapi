834f9241c60df456c27c23af4cf7227413d272b0
ffekirnew <ffekirnew0808@gmail.com>
2025-05-24 12:27:36 +0300

    Update sonarqube command

--- run_sonarqube.sh
+++ run_sonarqube.sh
@@ -5,18 +5,19 @@ PROJECT_BASE_DIR=sonarqube-fastapi
 SONARQUBE_PROJECT_KEY=fastapi
 SONARQUBE_TOKEN=sqp_f03c2bf4e9bd3319f6efb5139518338a3b318811
 
-pwd
-ls .
-mkdir -p /opt/sonar-scanner/conf
-ls /opt/sonar-scanner/conf
-cp sonar-project.properties /opt/sonar-scanner/conf/sonar-scanner.properties
-cat /opt/sonar-scanner/conf/sonar-scanner.properties
-
 docker run \
 		--network=host \
     --platform linux/amd64 \
 		-v $PWD:/$PROJECT_BASE_DIR \
 		-e SONAR_HOST_URL="$SONARQUBE_URL" \
-		-e SONAR_SCANNER_OPTS="-Dsonar.projectKey=$SONARQUBE_PROJECT_KEY -Dsonar.login=$SONARQUBE_TOKEN -Dsonar.projectBaseDir=/$PROJECT_BASE_DIR" \
-		sonarsource/sonar-scanner-cli	
+		-e SONAR_SCANNER_OPTS="-Dsonar.projectKey=$SONARQUBE_PROJECT_KEY \
+		-Dsonar.login=$SONARQUBE_TOKEN \
+		-Dsonar.coverage.dtdVerification=false \
+		-Dsonar.python.version=3.12 \
+		-Dsonar.python.coverage.reportPaths=/coverage.xml \
+		-Dsonar.python.xunit.reportPath=/result.xml \
+		-Dsonar.inclusions=/task_manager/**/*.py \
+		-Dsonar.projectBaseDir=/$PROJECT_BASE_DIR \
+		-Dsonar.coverage.exclusions=/**/__init__.py,/**/test_*.py" \
+		sonarsource/sonar-scanner-cli	 
 
